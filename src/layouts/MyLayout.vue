c149bba4e013822b248412d0e93686ad0d1178f4
Erandir <aefs12junior@gmail.com>
2019-04-06 17:31:39 -0300

    Template starting

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-layout>
+    <header-component
+      @handleState="handleState()"
+    />
+
+    <component
+      :is="sidebarComponent"
+      :miniState="miniState"
+      :leftDrawerOpen="leftDrawerOpen"
+      @setStateDrawer="setStateDrawer"
+    />
+
+    <q-page-container style="background-color: #EEEEEE !important;">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import HeaderComponent from '../components/template/header/HeaderComponent'
+import SidebarDesktopComponent from '../components/template/sidebar/SidebarDesktopComponent'
+import SidebarMobileComponent from '../components/template/sidebar/SidebarMobileComponent'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      miniState: false,
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      css: true,
+      sidebarComponent: this.$q.platform.is.desktop ? SidebarDesktopComponent : SidebarMobileComponent
+    }
+  },
+  methods: {
+    handleState () {
+      this.setStateDrawer(!this.miniState)
+    },
+    setStateDrawer (state) {
+      this.miniState = state
+    }
+  },
+  components: {
+    HeaderComponent,
+    SidebarDesktopComponent,
+    SidebarMobileComponent
+  }
+}
+</script>
+
+<style>
+  .my-header {
+    background-color: #1a1e34 !important;
+    color: #f5f5f5 !important;
+    min-height: 67px !important;
+  }
+  .q-layout-drawer-normal, .q-layout-drawer-mini {
+    background-color: #1a1e34 !important;
+    top: 86px !important;
+    transform: translateX(21px) !important;
+    height: 560px !important;
+    border-radius: 0px 20px;
+  }
+  .q-layout-drawer-mobile {
+    background-color: #1a1e34 !important;
+  }
+  .q-item-side {
+    color: white !important;
+  }
+  .q-item-label {
+    color: white !important;
+  }
+  .q-item-sublabel {
+    color: #a0a0a0 !important;
+  }
+  .q-item-separator-component {
+    margin: 1px 0px;
+  }
+  .q-layout-drawer-normal > .q-scrollarea-thumb {
+    background-color: #f5f5f5;
+  }
+</style>
