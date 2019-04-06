036c707c360f160186879409476dfd54f8b5882c
Erandir <aefs12junior@gmail.com>
2019-04-06 17:31:02 -0300

    Component sidebar to mobile

---
+++ src/components/template/sidebar/SidebarMobileComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <q-layout-drawer
+      v-model="localLeftDrawerOpen"
+      class="my-sidebar"
+    >
+      <menu-component/>
+    </q-layout-drawer>
+  </div>
+</template>
+
+<script>
+import MenuComponent from './MenuComponent'
+
+export default {
+  name: 'SidebarMobileComponent',
+  data () {
+    return {
+      localLeftDrawerOpen: ''
+    }
+  },
+  components: {
+    MenuComponent
+  },
+  props: {
+    miniState: {
+      required: true
+    }
+  },
+  watch: {
+    miniState: function (newVal) {
+      this.localLeftDrawerOpen = newVal
+    },
+    localLeftDrawerOpen: function (newVal) {
+      this.$emit('setStateDrawer', newVal)
+    }
+  },
+  mounted () {
+    this.localLeftDrawerOpen = this.miniState
+  }
+}
+</script>
+
+<style>
+</style>
