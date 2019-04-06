c9e6f9c6babd5b564efff7b0d9f4e6f6992b501f
Erandir <aefs12junior@gmail.com>
2019-04-06 17:30:50 -0300

    Component sidebar to desktop

---
+++ src/components/template/sidebar/SidebarDesktopComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <q-layout-drawer
+      :mini="localMiniState && localLeftDrawerOpen"
+      v-model="localMiniState"
+      style="color: white"
+      show-if-above
+    >
+      <menu-component/>
+    </q-layout-drawer>
+  </div>
+</template>
+
+<script>
+import MenuComponent from './MenuComponent'
+
+export default {
+  name: 'SidebarDesktopComponent',
+  data () {
+    return {
+      localLeftDrawerOpen: '',
+      localMiniState: ''
+    }
+  },
+  components: {
+    MenuComponent
+  },
+  props: {
+    miniState: {
+      required: true
+    },
+    leftDrawerOpen: {
+      required: true
+    }
+  },
+  watch: {
+    leftDrawerOpen: function (newVal) {
+      this.localLeftDrawerOpen = newVal
+    },
+    miniState: function (newVal) {
+      this.localMiniState = newVal
+    }
+  },
+  mounted () {
+    this.localMiniState = this.miniState
+    this.localLeftDrawerOpen = this.leftDrawerOpen
+  }
+}
+</script>
+
+<style>
+</style>
