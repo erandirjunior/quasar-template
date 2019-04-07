7e122663cf38e8dae5c205fb650bef76034510f3
Erandir <aefs12junior@gmail.com>
2019-04-07 14:51:08 -0300

    Added routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,23 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      {
+        path: '/form/fields',
+        component: () => import('pages/template/form/FieldPage.vue')
+      },
+      {
+        path: '/form/simple-form',
+        component: () => import('pages/template/form/SimpleFormPage.vue')
+      },
+      {
+        path: '/form/complex-form',
+        component: () => import('pages/template/form/ComplexFormPage.vue')
+      },
+      {
+        path: 'table/simple-table',
+        component: () => import('pages/template/table/SimpleTablePage.vue')
+      }
     ]
   }
 ]
