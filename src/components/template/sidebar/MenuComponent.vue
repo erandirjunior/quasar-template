8c595bad9f19d56b831743845a1cf46d1a21e48f
Erandir <aefs12junior@gmail.com>
2019-05-16 21:43:50 -0300

    Adding navigator menu

--- src/components/template/sidebar/MenuComponent.vue
+++ src/components/template/sidebar/MenuComponent.vue
@@ -5,53 +5,29 @@
         <q-item-side avatar="statics/icons/icon-192x192.png" class="my-class"/>
         <q-item-main label="Quasar template1"  />
       </q-item>
-      <q-item-separator color="white" />
     </div>
     <q-list
       no-border
       link
       inset-delimiter
     >
-      <q-item @click.native="openURL('http://quasar-framework.org')">
-        <q-item-main label="Docs" sublabel="quasar-framework.org" />
-        <q-item-side icon="school" />
-      </q-item>
-      <q-item @click.native="openURL('https://github.com/quasarframework/')">
-        <q-item-side icon="code" />
-        <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
-      </q-item>
-      <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-        <q-item-side icon="chat" />
-        <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-      </q-item>
-      <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-        <q-item-side icon="record_voice_over" />
-        <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-      </q-item>
-      <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-        <q-item-side icon="rss feed" />
-        <q-item-main label="Twitter" sublabel="@quasarframework" />
-      </q-item>
-      <q-item @click.native="openURL('http://quasar-framework.org')">
-        <q-item-main label="Docs" sublabel="quasar-framework.org" />
-        <q-item-side icon="school" />
-      </q-item>
-      <q-item @click.native="openURL('https://github.com/quasarframework/')">
-        <q-item-side icon="code" />
-        <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
-      </q-item>
-      <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-        <q-item-side icon="chat" />
-        <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-      </q-item>
-      <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-        <q-item-side icon="record_voice_over" />
-        <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-      </q-item>
-      <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-        <q-item-side icon="rss feed" />
-        <q-item-main label="Twitter" sublabel="@quasarframework" />
-      </q-item>
+      <q-collapsible
+        v-for="(menu, index) in menu"
+        :key="index"
+        :icon="menu.icon"
+        :label="menu.label"
+        class="my-collapsible"
+        v-model="menuOpen[index]"
+      >
+        <div @click="setOpenMenu(index)">
+          <q-item v-for="(submenu, subitem) in menu.submenu" :key="subitem" :to="submenu.router">
+            <q-item-side>
+              <q-icon name="subdirectory_arrow_right" size="20px" />
+            </q-item-side>
+            <q-item-main :label="submenu.label" class="q-body-10"/>
+          </q-item>
+        </div>
+      </q-collapsible>
     </q-list>
   </div>
 </template>
@@ -61,10 +37,54 @@ import { openURL } from 'quasar'
 export default {
   name: 'MenuComponent',
   data () {
-    return {}
+    return {
+      menuOpen: [],
+      menu: [
+        {
+          icon: 'insert_chart',
+          label: 'Forms',
+          submenu: [
+            {
+              label: 'Fields',
+              itens: null,
+              router: '/form/fields'
+            },
+            {
+              label: 'Simple Form',
+              itens: null,
+              router: '/form/simple-form'
+            },
+            {
+              label: 'Complex Form',
+              itens: null,
+              router: '/form/complex-form'
+            }
+          ]
+        },
+        {
+          icon: 'insert_chart',
+          label: 'Table',
+          submenu: [
+            {
+              label: 'SimpleTable',
+              itens: null,
+              router: '/table/simple-table'
+            }
+          ]
+        }
+      ]
+    }
   },
   methods: {
-    openURL
+    openURL,
+    setOpenMenu (position) {
+      this.menuOpen.length = 0
+      this.menuOpen.length = this.menu.length
+      this.$set(this.menuOpen, position, true)
+    }
+  },
+  created () {
+    this.menuOpen.length = this.menu.length
   }
 }
 </script>
@@ -72,5 +92,8 @@ export default {
 <style scoped>
   .q-item-separator-component:last-child {
     display: block;
+  }
+  .my-collapsible {
+    border-bottom: 1px solid white !important;
   }
 </style>
